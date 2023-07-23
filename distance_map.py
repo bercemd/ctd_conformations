8d157596c0e689bab8dfed0dbe1216bde739b007
Dutagaci Lab <98774280+bercemd@users.noreply.github.com>
2023-07-23 14:33:09 -0700

    Add files via upload

--- distance_map.py
+++ distance_map.py
@@ -1,47 +1,67 @@
-#This python script calculates the distance map of a given PDB file between residues of a protein.
+#This python script calculates the distance map between residues along a trajectory
+#and gives the average distance map
 #Modified by Amith, W. D. and Dutagaci, B. 2023, University of California Merced, Merced, CA 95343 USA.
 #Based on the examples in the user guide of MDAnalysis package
 #Reference:Michaud-Agrawal, N.; Denning, E. J.; Woolf, T. B.; Beckstein, O. MDAnalysis: A toolkit for the analysis of molecular dynamics simulations.
 #J.Comp.Chem 2011, 32 (10), 2319-2327. DOI: https://doi.org/10.1002/jcc.21787
-
 import MDAnalysis as mda
 from MDAnalysis.tests.datafiles import PDB_small
 from MDAnalysis.analysis import distances
-
 import numpy as np
-import matplotlib.pyplot as plt
+import os,sys
+import argparse
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    '--traj_path',type=str, default='md.dcd',
+    help='Trajectory path.')
+parser.add_argument(
+    '--psf_path',type=str, default='protein.psf',
+    help='PSF path.')
+parser.add_argument(
+    '--sela',type=str, default='protein',
+    help='First selection.')
+parser.add_argument(
+    '--selb',type=str, default='segid DNAA',
+    help='Second selection.')
+parser.add_argument(
+    '--out_path',type=str, default='rmsf.dat',
+    help='Output path.')
+arg = parser.parse_args()
 
 
-u = mda.Universe("protein.pdb")
 
-seq1_com = u.atoms.center_of_mass(compound='residues')
+def dist_mda(u,selecta,selectb):
+  sela = u.select_atoms(selecta)
+  selb = u.select_atoms(selectb)
 
-n_seq1 = len(seq1_com)
+  n_sela = len(sela)
+  n_selb = len(selb)
 
-print('sequence1 has {} residues'.format(n_seq1))
-res_dist = distances.self_distance_array(seq1_com)
-res_dist.shape
+  print('Sela has {} residues and Selb has {} residues'.format(n_sela, n_selb))
 
-sq_dist_res = np.zeros((n_seq1, n_seq1))
-triu = np.triu_indices_from(sq_dist_res, k=1)
-sq_dist_res[triu] = res_dist
-sq_dist_res.T[triu] = res_dist
+  sela_res = sela.residues
+  selb_res = selb.residues
+  sela_resids = sela_res.resids
+  selb_resids = selb_res.resids
+  dist_arr = np.zeros(shape=(len(sela_res), len(selb_res)))
+  boxsize = u.trajectory.ts.dimensions
+  for frame in u.trajectory:
+    for n in range(len(sela_res)):
+      for m in range(len(selb_res)):
+        distance = distances.distance_array(sela_res[n].atoms.positions,selb_res[m].atoms.positions,box=boxsize,backend='OpenMP')
+        min_dist = np.min(distance)
+        dist_arr[n,m] += min_dist
+  dist_arr = np.divide(dist_arr,len(u.trajectory))
+  return dist_arr, sela_resids, selb_resids
 
-vmin = 0
-vmax = 6
- 
-fig2, ax2 = plt.subplots()
-im2 = ax2.pcolormesh(u.residues.resids, u.residues.resids, sq_dist_res, shading='auto', vmin=vmin, vmax=vmax)
- 
-# plt.pcolor gives a rectangular grid by default
-# so we need to make our heatmap square
-ax2.set_aspect('equal')
- 
-# add figure labels and titles
-plt.ylabel('Residue IDs')
-plt.xlabel('Residue IDs')
- 
-# colorbar
-cbar2 = fig2.colorbar(im2)
-cbar2.ax.set_ylabel('Distance (Angstrom)')
-plt.show()
+
+U = mda.Universe(arg.psf_path, arg.traj_path, in_memory=True)
+dist_arr, sela_resids, selb_resids = dist_mda(U,arg.sela,arg.selb)
+dist_arr.shape
+outputfile = open(arg.out_path,"w")
+for i in range(len(dist_arr)):
+  for j in range(len(dist_arr[i])):
+    print(sela_resids[i],selb_resids[j],dist_arr[i][j],file=outputfile)
+outputfile.close()
